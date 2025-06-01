aa2a741f669f21bee9493a0faf5fc112abe60f2b
Dennis Ruppel <crap-dennis@gmx.net>
2025-06-01 23:15:38 +0200

    fix title for suggestions after video

--- AntiTranslate.user.js
+++ AntiTranslate.user.js
@@ -65,6 +65,7 @@
             .from(document.querySelectorAll(
                 'a[href*="/watch"] [role="text"], '
                 + 'a[href*="/watch"] [id="video-title"], '
+                + 'a[href*="/watch"] .ytp-videowall-still-info-title, '
                 + 'a[href*="/shorts"] [role="text"], '
                 + 'a[href*="/shorts"] [id="video-title"]'))
             .filter(a => { return a.textContent?.trim().length > 0; });
